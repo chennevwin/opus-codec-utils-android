6a0004a27ec0e3099a72c6a869465e6097fd2dfc
chennevwin <hanrong1.chen@tcl.com>
2026-05-11 10:30:17 +0800

    pref:优化opus源码库导入逻辑

--- opus/src/main/cpp/cmake/import_opus.cmake
+++ opus/src/main/cpp/cmake/import_opus.cmake
@@ -3,24 +3,11 @@ if (NOT TARGET Opus::opus)
     message(STATUS "import opus source code")
     include(FetchContent)
     set(OPUS_DOWNLOAD_URL https://github.com/xiph/opus/releases/download/v1.5.2/opus-1.5.2.tar.gz)
-    set(OPUS_PACKAGE_VERSION "1.5.2")
-    set(OPUS_PROJECT_VERSION "1.5.2")
 
     FetchContent_Declare(
-        opus
-        URL ${OPUS_DOWNLOAD_URL}
-        CMAKE_ARGS
-            -DBUILD_SHARED_LIBS=OFF
-            -DOPUS_BUILD_SHARED_LIBRARY=OFF
-            -DOPUS_BUILD_TESTING=OFF
-            -DOPUS_BUILD_PROGRAMS=OFF
-            -DOPUS_CUSTOM_MODES=ON
-            -DOPUS_DISABLE_INTRINSICS=OFF
-            -DOPUS_FIXED_POINT=OFF
-            -DOPUS_ENABLE_FLOAT_API=ON
-            -DOPUS_ASSERTIONS=OFF
-            -DOPUS_PACKAGE_VERSION=${OPUS_PACKAGE_VERSION}
-            -DOPUS_PROJECT_VERSION=${OPUS_PROJECT_VERSION}
+            opus
+            URL ${OPUS_DOWNLOAD_URL}
+            URL_HASH SHA256=65c1d2f78b9f2fb20082c38cbe47c951ad5839345876e46941612ee87f9a7ce1
     )
 
     FetchContent_MakeAvailable(opus)
